77bc6ae4e69b96336fd8e87a5a2df02006d86db8
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-06-01 00:38:33 +0545

    Implement models

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
 
-const events = [];
+const Event = require("./Models/Event");
 
 const app = express();
 
+//bodyParser middleware
 app.use(bodyParser.json());
+
+//database
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch(err => {
+    console.log("Error Occure", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello rod");
@@ -47,23 +60,37 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then(events => {
+            return events;
+          })
+          .catch(err => {
+            console.log(err);
+          });
       },
       createEvent: args => {
-        const event = {
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: args.eventInput.price,
-          date: args.eventInput.date
-        };
-        events.push(event);
-        return event;
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(event => {
+            console.log(event);
+            return event;
+          })
+          .catch(err => {
+            console.log(err);
+            throw err; //graphql returns error
+          });
       }
     },
     graphiql: true
   })
 );
 
-app.listen(3000,() => {
+app.listen(3000, () => {
   console.log("App listening in port 3000");
 });
